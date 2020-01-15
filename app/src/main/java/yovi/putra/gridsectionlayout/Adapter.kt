da618b9a398d9d56bb5de642efc00529ffe30a54
yovi.putra <yovi.putra@multipolar.com>
2020-01-15 20:05:31 +0700

    init

---
+++ app/src/main/java/yovi/putra/gridsectionlayout/Adapter.kt
@@ -0,0 +1,4 @@
+package yovi.putra.gridsectionlayout
+
+class Adapter {
+}
---
+++ app/src/main/java/yovi/putra/gridsectionlayout/Item.kt
@@ -0,0 +1,9 @@
+package yovi.putra.gridsectionlayout
+
+class Item (
+    var title: String?,
+    var type: Int = 0
+) {
+    val VIEW_TYPE_ITEM = 0
+    val VIEW_TYPE_HEADER = 1
+}
